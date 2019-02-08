ec249c0c180c8259c1ba2466b01ebcf18c99e67f
TheBluLion <41968802+TheBluLion@users.noreply.github.com>
2019-02-08 16:08:05 -0600

    Uploaded sample scripts

---
+++ Get-O365IpsRESTv2.ps1
@@ -0,0 +1,187 @@
+﻿<#
+    ############################################################################
+    The sample scripts are not supported under any Microsoft standard support
+    program or service. The sample scripts are provided AS IS without warranty
+    of any kind. Microsoft further disclaims all implied warranties including,
+    without limitation, any implied warranties of merchantability or of fitness
+    for a particular purpose. The entire risk arising out of the use or
+    performance of the sample scripts and documentation remains with you. In no
+    event shall Microsoft, its authors, or anyone else involved in the creation,
+    production, or delivery of the scripts be liable for any damages whatsoever
+    (including, without limitation, damages for loss of business profits,
+    business interruption, loss of business information, or other pecuniary
+    loss) arising out of the use of or inability to use the sample scripts or
+    documentation, even if Microsoft has been advised of the possibility of such
+    damages.
+    ############################################################################
+#>
+
+# webservice root URL
+$ws = "https://endpoints.office.com"
+
+# path where client ID and latest version number will be stored
+$datapath =  ".\O365IP_Script_settings.txt" #this is where settings are saved.
+
+# fetch settings if data file exists; otherwise create new file
+if (Test-Path $datapath) {
+    $content = Get-Content $datapath
+    $clientRequestId = $content[0]
+    $tenantName = $content[1]
+    $outputdir = $content[2]
+    $version = $content[3]
+}
+else {
+    Write-Host "Settings file not found. Generating new client id." -ForegroundColor Yellow
+    $clientRequestId = [GUID]::NewGuid().Guid
+    $tenantName = Read-Host "Enter tenant name"
+    $Outputdir = Read-Host "Enter output directory"
+    $version="0000000000"
+    @($clientRequestId, $tenantName,$outputdir,$version) | Out-File $datapath
+}
+
+$outputpath = $outputdir + "\Deltaoutput" + $(Get-Date -Format yyMMdd-hhmm) + ".xlsx" #this is where output is saved.
+
+function Get-BroadcastAddress{
+param(
+
+    [Parameter(Mandatory = $true)]
+	$ip,
+    [Parameter(Mandatory = $false)]
+	$cidr,
+    [Parameter(Mandatory = $false)]
+	$mask
+)
+
+    $ip = [Net.IPAddress]::Parse($ip)
+    $bytes = $ip.GetAddressBytes()
+
+
+    if($cidr){ #cidr provided
+        [int]$counter = $cidr
+        $maskbytes = @()
+        #build mask
+        foreach($octet in $bytes){
+            #get 8 bits
+            $maskOctet = ""
+                for ($i=0;$i -lt 8; $i++){
+                    if($counter -gt 0){
+                        $maskOctet = $maskOctet + "1"
+                        $counter--
+                    }
+                    else{
+                        $maskOctet = $maskOctet + "0"
+                    }
+                }
+            $maskbytes += [convert]::ToByte($maskOctet,2)
+        }
+    }
+
+
+    elseif($mask){ #mask provided instead
+        $mask = [Net.IPAddress]::Parse($mask)
+        $maskbytes = $mask.GetAddressBytes()
+    }
+
+
+    $broadcastbytes = @()
+    for($i=0;$i -lt $bytes.count; $i++){
+        $broadcastbytes += 255 -bxor $maskbytes[$i] -bor $bytes[$i]
+    }
+
+    if ($broadcastbytes.count -eq 4){
+        $broadcastip = $broadcastbytes -join (".")
+    }
+
+    elseif ($broadcastbytes.count -eq 16){
+        $broadcastip = ""
+        for($i=0;$i -lt $broadcastbytes.count; $i++){
+            $temp = $([convert]::ToString($broadcastbytes[$i],16))
+            if ($temp.length -eq 1){$temp = "0" + $temp}
+            if ($temp.length -eq 0){$temp = "00"}
+            $broadcastip =$broadcastip + $temp
+            if ($i % 2 -ne 0){$broadcastip =$broadcastip + ":"}
+        }
+        $broadcastip = $broadcastip.TrimEnd(":")
+    }
+$broadcastip = [Net.IPAddress]::Parse($broadcastip)
+return @($ip.IPAddressToString,$broadcastip.ipaddresstostring)
+}
+
+$EndpointSets = Invoke-RestMethod -Uri ($ws + "/endpoints/Worldwide?TenantName=$tenantName&clientRequestId=" + $clientRequestId)
+
+
+#region get full ip and url list
+[array]$export = ""
+[array]$urlexport = ""
+#list of ips
+    $iplist = $EndpointSets |?{$_.ips -notlike ""}
+    foreach($line in $iplist){
+        foreach ($ip in $line.ips){
+            $ipinfo = $ip -split "/"
+            $Ipinfo = Get-BroadcastAddress -ip $IpInfo[0] -cidr $ipinfo[1]
+            $port = (((("TCP-" + (($line.tcpports -split (",") | select -Unique) -join (",TCP-") ) + "," +"UDP-" + (($line.udpports -split (",") | select -Unique) -join (",UDP-") ) + ",").TrimEnd(",")).TrimEnd("TCP-")).TrimEnd("UDP-")).TrimEnd(",")
+            $export += $ip | select @{n='Service';e={$line.serviceAreaDisplayName}},@{n='CIDR';e={$ip}},@{n='StartIPRange';e={$ipinfo[0]}},@{n='EndIPRange';e={$ipinfo[1]}},@{n='Category';e={$line.category}},@{n='ExpressRoute';e={$line.expressRoute}},@{n='Required';e={$line.required}},@{n='Port';e={$port}},@{n='Notes';e={$line.notes}}
+        }
+    }
+    $export | Export-Excel $outputpath -WorkSheetname "FullIPList"
+
+#list of urls
+    $fulllist = $EndpointSets |?{$_.urls -notlike ""}
+    foreach($line in $fulllist){
+        foreach ($url in $line.urls){
+            $urlexport += $url | select @{n='Service';e={$line.serviceAreaDisplayName}},@{n='URL';e={$url}},@{n='Category';e={$line.category}},@{n='ExpressRoute';e={$line.expressRoute}},@{n='Required';e={$line.required}},@{n='Notes';e={$line.notes}}
+        }
+    }
+    $urlexport | Export-Excel $outputpath -WorkSheetname "FullURLList"
+
+
+$Changes = Invoke-RestMethod -Uri ($ws + "/changes/Worldwide/$version" + "?TenantName=$tenantName&clientRequestId=" + $clientRequestId)
+
+[array]$adds = $changes | ?{$_.add -notlike ""}
+[array]$removes = $changes | ?{$_.remove -notlike ""}
+
+#parse changes
+[array]$export = ""
+[array]$exporturls = ""
+    foreach($add in $adds){
+        $line = $EndpointSets |?{$_.id -eq $add.endpointsetid}
+        foreach($ip in $add.add.ips){
+            $IpInfo = $ip -split "/" 
+            $Ipinfo = Get-BroadcastAddress -ip $IpInfo[0] -cidr $ipinfo[1]
+            $port = (((("TCP-" + (($line.tcpports -split (",") | select -Unique) -join (",TCP-") ) + "," +"UDP-" + (($line.udpports -split (",") | select -Unique) -join (",UDP-") ) + ",").TrimEnd(",")).TrimEnd("TCP-")).TrimEnd("UDP-")).TrimEnd(",")
+            $export += $ip | select @{n='Service';e={$line.serviceAreaDisplayName}},@{n='CIDR';e={$ip}},@{n='StartIPRange';e={$ipinfo[0]}},@{n='EndIPRange';e={$ipinfo[1]}},@{n='Category';e={$line.category}},@{n='ExpressRoute';e={$line.expressRoute}},@{n='required';e={$line.required}},@{n='Port';e={$port}},@{n='EffectiveDate';e={$add.add.effectiveDate}}
+        foreach($url in $add.add.urls){
+            $exporturls += $url,$add.add.effectiveDate
+            }
+        }
+    }
+    $export | Sort-Object effectivedate| Export-Excel $outputpath -WorkSheetname "AddIPList"
+    $exporturls | Sort-Object effectivedate| Export-Excel $outputpath -WorkSheetname "AddURLList"
+
+[array]$export = ""
+[array]$exporturls = ""
+    foreach($remove in $removes){
+        $line = $EndpointSets |?{$_.id -eq $remove.endpointsetid}
+        foreach($ip in $remove.remove.ips){
+            if($EndpointSets.ips -notcontains $ip){
+                $IpInfo = $ip -split "/" 
+                $Ipinfo = Get-BroadcastAddress -ip $IpInfo[0] -cidr $ipinfo[1]
+                $port = (((("TCP-" + (($line.tcpports -split (",") | select -Unique) -join (",TCP-") ) + "," +"UDP-" + (($line.udpports -split (",") | select -Unique) -join (",UDP-") ) + ",").TrimEnd(",")).TrimEnd("TCP-")).TrimEnd("UDP-")).TrimEnd(",")
+                $export += $ip | select @{n='Service';e={$line.serviceAreaDisplayName}},@{n='CIDR';e={$ip}},@{n='StartIPRange';e={$ipinfo[0]}},@{n='EndIPRange';e={$ipinfo[1]}},@{n='Category';e={$line.category}},@{n='ExpressRoute';e={$line.expressRoute}},@{n='required';e={$line.required}},@{n='Port';e={$port}},@{n='EffectiveDate';e={$add.add.effectiveDate}}
+            }
+        }        
+        foreach($url in $remove.remove.urls){
+            $exporturls += $url,$remove.remove.effectivedate
+        }
+    }
+    $export | Sort-Object effectivedate| Export-Excel $outputpath -WorkSheetname "RemoveIPList"
+    $exporturls | Sort-Object effectivedate| Export-Excel $outputpath -WorkSheetname "RemoveURLList"
+
+
+#get current version and save to file.
+$version= (Invoke-RestMethod -Uri ($ws + "/version/Worldwide?TenantName=$tenantName&clientRequestId=" + $clientRequestId)).latest
+@($clientRequestId, $tenantName,$outputdir,$version) | Out-File $datapath
+
+#open file and exit
+Read-Host "Press enter to open output file"
+explorer $outputpath
---
+++ Graph-AllTeams.ps1
@@ -0,0 +1,40 @@
+﻿Function GetAccessToken
+   {
+    param (        
+        [Parameter(Position=0, Mandatory=$false)] 
+        [string] $Office365Username, 
+        [Parameter(Position=1, Mandatory=$false)] 
+        [string] $Office365Password
+      )
+    # Add ADAL (Microsoft.IdentityModel.Clients.ActiveDirectory.dll) assembly path from Azure Resource Manager SDK location
+    #Add-Type -Path "C:\Program Files (x86)\Microsoft SDKs\Azure\PowerShell\ResourceManager\AzureResourceManager\AzureRM.Profile\Microsoft.IdentityModel.Clients.ActiveDirectory.dll"
+    # or simply import AzureRm module using below command
+     Import-Module AzureRm
+    #PowerShell Client Id. This is a well known Azure AD client id of PowerShell client. You don't need to create an Azure AD app.
+    $clientId = "1950a258-227b-4e31-a9cf-717495945fc2"
+    $redirectUri = "urn:ietf:wg:oauth:2.0:oob"
+    $resourceURI = "https://graph.microsoft.com"
+    $authority = "https://login.microsoftonline.com/common"
+    $authContext = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.AuthenticationContext" -ArgumentList $authority
+     
+    if (([string]::IsNullOrEmpty($Office365Username) -eq $false) -and ([string]::IsNullOrEmpty($Office365Password) -eq $false)) 
+    { 
+    $SecurePassword = ConvertTo-SecureString -AsPlainText $Office365Password -Force           
+    #Build Azure AD credentials object  
+    $AADCredential = New-Object "Microsoft.IdentityModel.Clients.ActiveDirectory.UserCredential" -ArgumentList $Office365Username,$SecurePassword
+    # Get token without login prompts.
+    $authResult = $authContext.AcquireToken($resourceURI, $clientId,$AADCredential)
+    } 
+    else 
+    {     
+    # Get token by prompting login window.
+    $authResult = $authContext.AcquireToken($resourceURI, $clientId, $redirectUri, [Microsoft.IdentityModel.Clients.ActiveDirectory.PromptBehavior]::Always)
+    } 
+    return $authResult.AccessToken
+}
+
+$token = GetAccessToken
+
+$apiUrl = 'https://graph.microsoft.com/beta/groups?$filter=resourceProvisioningOptions/Any(x:x eq ''Team'')'
+
+$Teams = Invoke-RestMethod -Headers @{Authorization = "Bearer $token"} -Uri $apiUrl -Method Get
